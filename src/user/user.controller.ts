96302f1b8cea38d72eb696f9c0dc52a50eba9cb3
Robert Koch <robertkoch117@gmail.com>
2024-08-08 09:26:33 -0300

    Interceptors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Put, UseInterceptors } from "@nestjs/common";
 import { Delete, Patch } from "@nestjs/common/decorators/http";
+import { LogInterceptor } from "src/interceptors/log.interceptor";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
 import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
 import { UserService } from "./user.service";
 
+@UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
 
