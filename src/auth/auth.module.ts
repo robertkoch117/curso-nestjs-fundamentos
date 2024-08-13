2d2178f6f5e4481c76e856a4ad05df5e4c702a6a
Robert Koch <robertkoch117@gmail.com>
2024-08-13 08:26:56 -0300

    JWT - AuthController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { UserModule } from "src/user/user.module";
+import { AuthController } from "./auth.controller";
 
 @Module({
-    imports: [JwtModule.register({
-        secret: "N&IzU$V?ABqT.8-32jWq<:D@&DEM}J>D"
-    })]
+    imports: [
+        JwtModule.register({
+            secret: "N&IzU$V?ABqT.8-32jWq<:D@&DEM}J>D"
+        }),
+        UserModule,
+        PrismaModule
+    ],
+    controllers: [AuthController]
 })
 export class AuthModule{
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
-import {Injectable} from '@nestjs/common'
+import {Injectable, UnauthorizedException} from '@nestjs/common'
 import {JwtService} from '@nestjs/jwt'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly jwtService: JwtService){}
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly prisma: PrismaService
+    ){}
 
     async createToken(){
         // return this.jwtService.sign();
@@ -12,5 +16,54 @@ export class AuthService {
 
     async checkToken(token: string){
         // return this.jwtService.verify()
+    }
+
+    async login(email:string, password:string){
+        
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email,
+                password
+            }
+        });
+
+        if(!user){
+            throw new UnauthorizedException('Email e/ou senha incorretos.');
+        }
+
+        return user;
+    }
+
+    async forget(email: string){
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email
+            }
+        });
+
+        if(!user){
+            throw new UnauthorizedException('E-mail está incorreto.');
+        }
+
+        //TO DO: Enviar o e-mail...
+        return true;
+    }
+
+    async reset(password:string, token:string){
+
+        //TO DO: Validar o token...
+        const id = 0;
+        
+        await this.prisma.user.update({
+            where: {
+                id
+            },
+            data:{
+                password,
+            }
+        });
+
+        return true;
+
     }
 }
