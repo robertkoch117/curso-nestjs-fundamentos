b475ee93685d0ec72e7eba743248b5678b58e54f
Robert Koch <robertkoch117@gmail.com>
2024-08-16 09:41:17 -0300

    Proteção Ataque de Força Bruta (Rate Limiting)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core/constants';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard } from '@nestjs/throttler/dist/throttler.guard';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,11 +9,18 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 100
+    }]),
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule)
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
   exports: [AppService]
 })
 export class AppModule {}
