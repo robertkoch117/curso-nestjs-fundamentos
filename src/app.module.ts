71514763f6f410bb42c2e510bf78d8080f148b02
Robert Koch <robertkoch117@gmail.com>
2025-01-25 10:02:59 -0300

    Mailer Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core/constants';
@@ -7,7 +8,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -16,7 +17,21 @@ import { UserModule } from './user/user.module';
       limit: 100
     }]),
     forwardRef(() => UserModule),
-    forwardRef(() => AuthModule)
+    forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: 'smtps://king.runolfsdottir70@ethereal.email:FvGxA8Av1haASCMrdp@smtp.ethereal.email',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+
   ],
   controllers: [AppController],
   providers: [AppService, {
